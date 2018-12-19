d24b43f583c1d2a026df54ae7678fd30ec9eedf2
Herwig Hochleitner <herwig@bendlas.net>
2018-12-20 00:08:44 +0100

    pythonPackages.azure-mgmt-storage: 3.0.0 -> 3.1.0
    3.0.0 produced empty output

--- pkgs/development/python-modules/azure-mgmt-storage/default.nix
+++ pkgs/development/python-modules/azure-mgmt-storage/default.nix
@@ -6,24 +6,18 @@
 }:
 
 buildPythonPackage rec {
-  version = "3.0.0";
+  version = "3.1.0";
   pname = "azure-mgmt-storage";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "24c52b9dcb5e224ca9572d6ec39b53d332bdfe01818e85ec1cc1b5bedf16ce07";
+    sha256 = "0yjwi9xqqw9009h55jvnsxll71xg3ki10lfp0h0zgw4apfdpljw5";
   };
 
-  preConfigure = ''
-    # Patch to make this package work on requests >= 2.11.x
-    # CAN BE REMOVED ON NEXT PACKAGE UPDATE
-    sed -i 's|len(request_content)|str(len(request_content))|' azure/mgmt/storage/storagemanagement.py
-  '';
-
   postInstall = ''
-    echo "__import__('pkg_resources').declare_namespace(__name__)" >> "$out/lib/${python.libPrefix}"/site-packages/azure/__init__.py
-    echo "__import__('pkg_resources').declare_namespace(__name__)" >> "$out/lib/${python.libPrefix}"/site-packages/azure/mgmt/__init__.py
+    echo "__import__('pkg_resources').declare_namespace(__name__)" >> $out/lib/${python.libPrefix}/site-packages/azure/__init__.py
+    echo "__import__('pkg_resources').declare_namespace(__name__)" >> $out/lib/${python.libPrefix}/site-packages/azure/mgmt/__init__.py
   '';
 
   propagatedBuildInputs = [ azure-mgmt-common ];
