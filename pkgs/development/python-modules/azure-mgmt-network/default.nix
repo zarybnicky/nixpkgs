5a64d8e8fc7b08908fd4e5ef19b1c3711445fec6
Herwig Hochleitner <herwig@bendlas.net>
2018-12-20 00:09:40 +0100

    pythonPackages.azure-mgmt-network: 2.2.1 -> 2.4.0
    2.2.1 produced empty output

--- pkgs/development/python-modules/azure-mgmt-network/default.nix
+++ pkgs/development/python-modules/azure-mgmt-network/default.nix
@@ -6,20 +6,14 @@
 }:
 
 buildPythonPackage rec {
-  version = "2.2.1";
+  version = "2.4.0";
   pname = "azure-mgmt-network";
 
   src = fetchPypi {
     inherit pname version;
     extension = "zip";
-    sha256 = "a4327bccc435ca4f829ac18f82f17923b490958c202af7a86044ccabeaaa5401";
+    sha256 = "19h8fjw5ficbqvz1kzhgf3aqs729hs3haayn2qrg2ny53q9irh9p";
   };
-
-  preConfigure = ''
-    # Patch to make this package work on requests >= 2.11.x
-    # CAN BE REMOVED ON NEXT PACKAGE UPDATE
-    sed -i 's|len(request_content)|str(len(request_content))|' azure/mgmt/network/networkresourceprovider.py
-  '';
 
   postInstall = ''
     echo "__import__('pkg_resources').declare_namespace(__name__)" >> "$out/lib/${python.libPrefix}"/site-packages/azure/__init__.py
